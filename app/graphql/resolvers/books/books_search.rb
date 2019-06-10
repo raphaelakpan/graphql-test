04e6e1d98d751b47f684c061974ca8b2d8a6fae0
Raphael Akpan <raphael.akpan@andela.com>
2019-06-10 12:31:34 +0100

    Add search filters for books

--- app/graphql/resolvers/books/books_search.rb
+++ app/graphql/resolvers/books/books_search.rb
@@ -1,17 +1,41 @@
 module Resolvers
   module Books
     class BooksSearch < BaseSearchResolver
+      class BookFilter < Types::BaseInputObject
+        argument :OR, [self], required: false
+        argument :title_contains, String, required: false
+        argument :title_equals, String, required: false
+        argument :genre_equals, Types::Enums::Genre, required: false
+      end
+
       description "Finds all books"
 
-      scope { ::Book.all }
+      scope { get_scope }
 
-      # type types[Types::Custom::Book]
       type types[Types::Custom::Book]
 
-      option :title, type: types.String, with: :filter_by_title
+      option :filter, type: BookFilter, with: :apply_filter
 
-      def filter_by_title(scope, value)
-        scope.where(title: value)
+      def apply_filter(scope, value)
+        branches = normalize_filters(value).reduce { |a, b| a.or(b) }
+        scope.merge(branches)
+      end
+
+      def normalize_filters(value, branches = [])
+        scope = get_scope
+        scope = scope.title_contains(value[:title_contains]) if value[:title_contains].present?
+        scope = scope.title_equals(value[:title_equals]) if value[:title_equals].present?
+        scope = scope.genre_equals(value[:genre_equals]) if value[:genre_equals].present?
+
+        branches.push(scope)
+
+        value[:OR].reduce(branches) { |b, v| normalize_filters(v, b) } if value[:OR].present?
+
+        branches
+      end
+
+      def get_scope
+        ::Book.all
       end
     end
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  scope :title_contains, -> (value) { where("title ILIKE ?", "%#{value}%") }
+  scope :title_equals, -> (value) { where("LOWER(title) = ?", value.downcase) }
+  scope :genre_equals, -> (value) { where(genre: value) }
 end
